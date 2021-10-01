eb4dcf6d00dc10001fd7b321855198378e2f0782
Bill McKessy <bill@192.168.0.100>
2021-10-01 14:01:39 +0200

    ok

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import { Provider } from "react-redux"
-// import { useMediaQuery } from 'react-responsive'
 import store from "./js/store/index"
 import Application from "./js/components/App"
-
 
 function App() {
   return (
